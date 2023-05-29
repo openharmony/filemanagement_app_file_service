30d1f97a60d12b636e6626163cab86f5e5cf6bc5
huaqingsimeng <1004904143@qq.com>
2023-05-29 19:42:26 +0800

    修改find查找方式 改为start_with
    Signed-off-by: huaqingsimeng <qiukaiqing@huawei.com>

--- utils/src/b_error/b_excep_utils.cpp
+++ utils/src/b_error/b_excep_utils.cpp
@@ -30,8 +30,8 @@ void BExcepUltils::VerifyPath(const string_view &path, bool isExtension)
         auto ret = canonicalize(path.data());
         string absPath = ret.c_str();
         if (isExtension &&
-            absPath.find(string(BConstants::PATH_BUNDLE_BACKUP_HOME)
-            .append(BConstants::SA_BUNDLE_BACKUP_RESTORE)) == std::string::npos) {
+            !absPath.startswith(
+                string(BConstants::PATH_BUNDLE_BACKUP_HOME).append(BConstants::SA_BUNDLE_BACKUP_RESTORE))) {
             throw BError(BError::Codes::EXT_INVAL_ARG, "Invalid path, not in backup restore path");
         }
     } catch (const rust::Error &e) {
