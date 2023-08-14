c4c9b20531fe3fc3209ca11e200cf4c173ab42e9
lvyuanyuan <lvyuanyuan7@huawei.com>
2023-08-14 03:26:59 +0000

    告警清理
    Signed-off-by: lvyuanyuan <lvyuanyuan7@huawei.com>
    Change-Id: I9ce8dbb6e6a9a16d2206278f1c366c817682ec7d

--- interfaces/common/src/sandbox_helper.cpp
+++ interfaces/common/src/sandbox_helper.cpp
@@ -245,10 +245,10 @@ int32_t SandboxHelper::GetPhysicalPath(const std::string &fileUri, const std::st
         GetSandboxPathMap(sandboxPathMap_);
     }
 
-    int32_t curPrefixMatchLen = 0;
+    size_t curPrefixMatchLen = 0;
     for (auto it = sandboxPathMap_.begin(); it != sandboxPathMap_.end(); it++) {
         string sandboxPathPrefix = it->first;
-        int32_t prefixMatchLen = sandboxPathPrefix.length();
+        size_t prefixMatchLen = sandboxPathPrefix.length();
         if (sandboxPath.length() >= prefixMatchLen) {
             string sandboxPathTemp = sandboxPath.substr(0, prefixMatchLen);
             if (sandboxPathTemp == sandboxPathPrefix && curPrefixMatchLen <= prefixMatchLen) {
