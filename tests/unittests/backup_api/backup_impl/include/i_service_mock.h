e2b4229a60528da5fec28ee882c526540c9a857a
yaoruozi1@huawei.com <yaoruozi1@huawei.com>
2024-03-31 23:59:14 +0800

    备份恢复框架增加恢复事件下发
    Signed-off-by: yaoruozi1@huawei.com <yaoruozi1@huawei.com>

--- tests/unittests/backup_api/backup_impl/include/i_service_mock.h
+++ tests/unittests/backup_api/backup_impl/include/i_service_mock.h
@@ -106,7 +106,7 @@ public:
         return BError(BError::Codes::OK);
     }
 
-    ErrCode AppendBundlesBackupSession(UniqueFd fd,
+    ErrCode AppendBundlesRestoreSession(UniqueFd fd,
                                         const std::vector<BundleName> &bundleNames,
                                         const std::vector<std::string> &detailInfos,
                                         RestoreTypeEnum restoreType,
