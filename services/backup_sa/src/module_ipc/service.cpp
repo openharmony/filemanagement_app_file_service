fa8c34c6b3f6bb66198a7c3841f07f5386fc52f2
libuyan <libuyan1@huawei.com>
2024-04-10 20:57:14 +0800

    add target
    Signed-off-by: libuyan <libuyan1@huawei.com>

--- services/backup_sa/src/module_ipc/service.cpp
+++ services/backup_sa/src/module_ipc/service.cpp
@@ -938,6 +938,7 @@ ErrCode Service::GetBackupInfo(BundleName &bundleName, std::string &result)
     try {
         HILOGI("Service::GetBackupInfo begin.");
         session_->IncreaseSessionCnt();
+        session_->SetSessionUserId(GetUserIdDefault());
         auto backupConnection = session_->CreateBackupConnection(bundleName);
         auto callConnDone = [ptr {wptr(this)}](const string &&bundleName) {
             HILOGI("callConnDone begin.");
