d9d3be71173d8a108cf21a625567626066cfbfec
wangpggg <wangpeng477@huawei.com>
2024-06-11 14:45:56 +0800

    add service_stub fuzz interfaces
    Signed-off-by: wangpeng <wangpeng477@huawei.com>

--- test/fuzztest/backupext_fuzzer/backupext_fuzzer.cpp
+++ test/fuzztest/backupext_fuzzer/backupext_fuzzer.cpp
@@ -168,6 +168,119 @@ bool SetCreatorFuzzTest(shared_ptr<ExtBackup> backup, const uint8_t *data, size_
     backup->SetCreator(creator);
     return true;
 }
+
+bool CmdGetFileHandleFuzzTest(shared_ptr<BackupExtExtension> extension, const uint8_t *data, size_t size)
+{
+    MessageParcel msg;
+    MessageParcel reply;
+
+    msg.WriteString(string(reinterpret_cast<const char*>(data), size));
+    extension->CmdGetFileHandle(msg, reply);
+    return true;
+}
+
+bool CmdHandleClearFuzzTest(shared_ptr<BackupExtExtension> extension, const uint8_t *data, size_t size)
+{
+    MessageParcel msg;
+    MessageParcel reply;
+
+    extension->CmdHandleClear(msg, reply);
+    return true;
+}
+
+bool CmdHandleBackupFuzzTest(shared_ptr<BackupExtExtension> extension, const uint8_t *data, size_t size)
+{
+    MessageParcel msg;
+    MessageParcel reply;
+
+    extension->CmdHandleBackup(msg, reply);
+    return true;
+}
+
+bool CmdPublishFileFuzzTest(shared_ptr<BackupExtExtension> extension, const uint8_t *data, size_t size)
+{
+    MessageParcel msg;
+    MessageParcel reply;
+
+    msg.WriteString(string(reinterpret_cast<const char*>(data), size));
+    extension->CmdPublishFile(msg, reply);
+    return true;
+}
+
+bool CmdHandleRestoreFuzzTest(shared_ptr<BackupExtExtension> extension, const uint8_t *data, size_t size)
+{
+    MessageParcel msg;
+    MessageParcel reply;
+
+    extension->CmdHandleRestore(msg, reply);
+    return true;
+}
+
+bool CmdGetIncrementalFileHandleFuzzTest(shared_ptr<BackupExtExtension> extension, const uint8_t *data, size_t size)
+{
+    MessageParcel msg;
+    MessageParcel reply;
+
+    msg.WriteString(string(reinterpret_cast<const char*>(data), size));
+    extension->CmdGetIncrementalFileHandle(msg, reply);
+    return true;
+}
+
+bool CmdPublishIncrementalFileFuzzTest(shared_ptr<BackupExtExtension> extension, const uint8_t *data, size_t size)
+{
+    MessageParcel msg;
+    MessageParcel reply;
+
+    msg.WriteString(string(reinterpret_cast<const char*>(data), size));
+    extension->CmdPublishIncrementalFile(msg, reply);
+    return true;
+}
+
+bool CmdHandleIncrementalBackupFuzzTest(shared_ptr<BackupExtExtension> extension, const uint8_t *data, size_t size)
+{
+    if (data == nullptr || size < sizeof(int) + sizeof(int)) {
+        return true;
+    }
+
+    MessageParcel msg;
+    MessageParcel reply;
+
+    int pos = 0;
+    int incrementalFd = TypeCast<int>(data, &pos);
+    int manifestFd = TypeCast<int>(data + pos);
+    msg.WriteFileDescriptor(incrementalFd);
+    msg.WriteFileDescriptor(manifestFd);
+    extension->CmdPublishIncrementalFile(msg, reply);
+    return true;
+}
+
+bool CmdIncrementalOnBackupFuzzTest(shared_ptr<BackupExtExtension> extension, const uint8_t *data, size_t size)
+{
+    MessageParcel msg;
+    MessageParcel reply;
+
+    extension->CmdIncrementalOnBackup(msg, reply);
+    return true;
+}
+
+bool CmdGetIncrementalBackupFileHandleFuzzTest(shared_ptr<BackupExtExtension> extension, const uint8_t *data,
+    size_t size)
+{
+    MessageParcel msg;
+    MessageParcel reply;
+
+    extension->CmdGetIncrementalBackupFileHandle(msg, reply);
+    return true;
+}
+
+bool CmdGetBackupInfoFuzzTest(shared_ptr<BackupExtExtension> extension, const uint8_t *data, size_t size)
+{
+    MessageParcel msg;
+    MessageParcel reply;
+
+    extension->CmdGetBackupInfo(msg, reply);
+    return true;
+}
 } // namespace OHOS
 
 /* Fuzzer entry point */
@@ -193,5 +306,21 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
     OHOS::CallExtRestoreFuzzTest(extBackup, data, size);
     OHOS::SetCreatorFuzzTest(extBackup, data, size);
 
+    try {
+        OHOS::CmdGetFileHandleFuzzTest(extension, data, size);
+        OHOS::CmdHandleClearFuzzTest(extension, data, size);
+        OHOS::CmdHandleBackupFuzzTest(extension, data, size);
+        OHOS::CmdPublishFileFuzzTest(extension, data, size);
+        OHOS::CmdHandleRestoreFuzzTest(extension, data, size);
+        OHOS::CmdGetIncrementalFileHandleFuzzTest(extension, data, size);
+        OHOS::CmdPublishIncrementalFileFuzzTest(extension, data, size);
+        OHOS::CmdHandleIncrementalBackupFuzzTest(extension, data, size);
+        OHOS::CmdIncrementalOnBackupFuzzTest(extension, data, size);
+        OHOS::CmdGetIncrementalBackupFileHandleFuzzTest(extension, data, size);
+        OHOS::CmdGetBackupInfoFuzzTest(extension, data, size);
+    } catch (OHOS::FileManagement::Backup::BError &err) {
+        // Only filter BError errors, Other results are not expected.
+    }
+    
     return 0;
 }
