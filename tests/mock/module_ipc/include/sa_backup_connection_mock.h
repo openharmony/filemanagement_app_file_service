203ec41195c341305f6f76dc129581189d08d824
wangpggg <wangpeng477@huawei.com>
2024-09-06 09:24:35 +0800

    add ut
    Signed-off-by: wangpeng <wangpeng477@huawei.com>

---
+++ tests/mock/module_ipc/include/sa_backup_connection_mock.h
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) 2024 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License") = 0;
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef OHOS_FILEMGMT_BACKUP_SA_BACKUP_CONNECTION_MOCK_H
+#define OHOS_FILEMGMT_BACKUP_SA_BACKUP_CONNECTION_MOCK_H
+
+#include <gmock/gmock.h>
+
+#include "module_ipc/sa_backup_connection.h"
+
+namespace OHOS::FileManagement::Backup {
+class BSABackupConnection : public RefBase {
+public:
+    virtual bool InputParaSet(MessageParcel&) = 0;
+    virtual bool OutputParaGet(MessageParcel&) = 0;
+    virtual ErrCode ConnectBackupSAExt(std::string, std::string, std::string) = 0;
+    virtual ErrCode DisconnectBackupSAExt() = 0;
+    virtual bool IsSAExtConnected() = 0;
+    virtual sptr<ILocalAbilityManager> GetBackupSAExtProxy() = 0;
+    virtual ErrCode LoadBackupSAExt() = 0;
+    virtual ErrCode LoadBackupSAExtInner() = 0;
+    virtual ErrCode CallBackupSA() = 0;
+    virtual ErrCode CallRestoreSA(UniqueFd) = 0;
+public:
+    BSABackupConnection() = default;
+    virtual ~BSABackupConnection() = default;
+public:
+    static inline sptr<BSABackupConnection> saConnect = nullptr;
+};
+
+class SABackupConnectionMock : public BSABackupConnection {
+public:
+    MOCK_METHOD(bool, InputParaSet, (MessageParcel&));
+    MOCK_METHOD(bool, OutputParaGet, (MessageParcel&));
+    MOCK_METHOD(ErrCode, ConnectBackupSAExt, (std::string, std::string, std::string));
+    MOCK_METHOD(ErrCode, DisconnectBackupSAExt, ());
+    MOCK_METHOD(bool, IsSAExtConnected, ());
+    MOCK_METHOD((sptr<ILocalAbilityManager>), GetBackupSAExtProxy, ());
+    MOCK_METHOD(ErrCode, LoadBackupSAExt, ());
+    MOCK_METHOD(ErrCode, LoadBackupSAExtInner, ());
+    MOCK_METHOD(ErrCode, CallBackupSA, ());
+    MOCK_METHOD(ErrCode, CallRestoreSA, (UniqueFd));
+};
+} // namespace OHOS::FileManagement::Backup
+#endif // OHOS_FILEMGMT_BACKUP_SA_BACKUP_CONNECTION_MOCK_H
---
+++ tests/mock/module_ipc/include/service_stub_mock.h
@@ -0,0 +1,98 @@
+/*
+ * Copyright (c) 2024 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef OHOS_FILEMGMT_BACKUP_SERVICE_STUB_MOCK_H
+#define OHOS_FILEMGMT_BACKUP_SERVICE_STUB_MOCK_H
+
+#include <gmock/gmock.h>
+
+namespace OHOS::FileManagement::Backup {
+class BServiceStub {
+public:
+    virtual int32_t OnRemoteRequest(uint32_t, MessageParcel&, MessageParcel&, MessageOption&) = 0;
+    virtual int32_t CmdInitRestoreSession(MessageParcel&, MessageParcel&) = 0;
+    virtual int32_t CmdInitBackupSession(MessageParcel&, MessageParcel&) = 0;
+    virtual int32_t CmdStart(MessageParcel&, MessageParcel&) = 0;
+    virtual int32_t CmdGetLocalCapabilities(MessageParcel&, MessageParcel&) = 0;
+    virtual int32_t CmdPublishFile(MessageParcel&, MessageParcel&) = 0;
+    virtual int32_t CmdAppFileReady(MessageParcel&, MessageParcel&) = 0;
+    virtual int32_t CmdAppDone(MessageParcel&, MessageParcel&) = 0;
+    virtual int32_t CmdResultReport(MessageParcel&, MessageParcel&) = 0;
+    virtual int32_t CmdGetFileHandle(MessageParcel&, MessageParcel&) = 0;
+    virtual int32_t CmdAppendBundlesRestoreSession(MessageParcel&, MessageParcel&) = 0;
+    virtual int32_t CmdAppendBundlesDetailsRestoreSession(MessageParcel&, MessageParcel&) = 0;
+    virtual int32_t CmdAppendBundlesBackupSession(MessageParcel&, MessageParcel&) = 0;
+    virtual int32_t CmdAppendBundlesDetailsBackupSession(MessageParcel&, MessageParcel&) = 0;
+    virtual int32_t CmdFinish(MessageParcel&, MessageParcel&) = 0;
+    virtual int32_t CmdRelease(MessageParcel&, MessageParcel&) = 0;
+    virtual int32_t CmdGetLocalCapabilitiesIncremental(MessageParcel&, MessageParcel&) = 0;
+    virtual int32_t CmdGetAppLocalListAndDoIncrementalBackup(MessageParcel&, MessageParcel&) = 0;
+    virtual int32_t CmdInitIncrementalBackupSession(MessageParcel&, MessageParcel&) = 0;
+    virtual int32_t CmdAppendBundlesIncrementalBackupSession(MessageParcel&, MessageParcel&) = 0;
+    virtual int32_t CmdAppendBundlesDetailsIncrementalBackupSession(MessageParcel&, MessageParcel&) = 0;
+    virtual int32_t CmdPublishIncrementalFile(MessageParcel&, MessageParcel&) = 0;
+    virtual int32_t CmdPublishSAIncrementalFile(MessageParcel&, MessageParcel&) = 0;
+    virtual int32_t CmdAppIncrementalFileReady(MessageParcel&, MessageParcel&) = 0;
+    virtual int32_t CmdAppIncrementalDone(MessageParcel&, MessageParcel&) = 0;
+    virtual int32_t CmdGetIncrementalFileHandle(MessageParcel&, MessageParcel&) = 0;
+    virtual int32_t CmdGetBackupInfo(MessageParcel&, MessageParcel&) = 0;
+    virtual int32_t CmdUpdateTimer(MessageParcel&, MessageParcel&) = 0;
+    virtual int32_t CmdUpdateSendRate(MessageParcel&, MessageParcel&) = 0;
+    virtual void ServiceStubSupplement() = 0;
+    virtual void ServiceStubSuppAppendBundles() = 0;
+
+public:
+    BServiceStub() = default;
+    virtual ~BServiceStub() = default;
+public:
+    static inline std::shared_ptr<BServiceStub> stub = nullptr;
+};
+
+class ServiceStubMock : public BServiceStub {
+public:
+    MOCK_METHOD(int32_t, OnRemoteRequest, (uint32_t, MessageParcel&, MessageParcel&, MessageOption&));
+    MOCK_METHOD(int32_t, CmdInitRestoreSession, (MessageParcel&, MessageParcel&));
+    MOCK_METHOD(int32_t, CmdInitBackupSession, (MessageParcel&, MessageParcel&));
+    MOCK_METHOD(int32_t, CmdStart, (MessageParcel&, MessageParcel&));
+    MOCK_METHOD(int32_t, CmdGetLocalCapabilities, (MessageParcel&, MessageParcel&));
+    MOCK_METHOD(int32_t, CmdPublishFile, (MessageParcel&, MessageParcel&));
+    MOCK_METHOD(int32_t, CmdAppFileReady, (MessageParcel&, MessageParcel&));
+    MOCK_METHOD(int32_t, CmdAppDone, (MessageParcel&, MessageParcel&));
+    MOCK_METHOD(int32_t, CmdResultReport, (MessageParcel&, MessageParcel&));
+    MOCK_METHOD(int32_t, CmdGetFileHandle, (MessageParcel&, MessageParcel&));
+    MOCK_METHOD(int32_t, CmdAppendBundlesRestoreSession, (MessageParcel&, MessageParcel&));
+    MOCK_METHOD(int32_t, CmdAppendBundlesDetailsRestoreSession, (MessageParcel&, MessageParcel&));
+    MOCK_METHOD(int32_t, CmdAppendBundlesBackupSession, (MessageParcel&, MessageParcel&));
+    MOCK_METHOD(int32_t, CmdAppendBundlesDetailsBackupSession, (MessageParcel&, MessageParcel&));
+    MOCK_METHOD(int32_t, CmdFinish, (MessageParcel&, MessageParcel&));
+    MOCK_METHOD(int32_t, CmdRelease, (MessageParcel&, MessageParcel&));
+    MOCK_METHOD(int32_t, CmdGetLocalCapabilitiesIncremental, (MessageParcel&, MessageParcel&));
+    MOCK_METHOD(int32_t, CmdGetAppLocalListAndDoIncrementalBackup, (MessageParcel&, MessageParcel&));
+    MOCK_METHOD(int32_t, CmdInitIncrementalBackupSession, (MessageParcel&, MessageParcel&));
+    MOCK_METHOD(int32_t, CmdAppendBundlesIncrementalBackupSession, (MessageParcel&, MessageParcel&));
+    MOCK_METHOD(int32_t, CmdAppendBundlesDetailsIncrementalBackupSession, (MessageParcel&, MessageParcel&));
+    MOCK_METHOD(int32_t, CmdPublishIncrementalFile, (MessageParcel&, MessageParcel&));
+    MOCK_METHOD(int32_t, CmdPublishSAIncrementalFile, (MessageParcel&, MessageParcel&));
+    MOCK_METHOD(int32_t, CmdAppIncrementalFileReady, (MessageParcel&, MessageParcel&));
+    MOCK_METHOD(int32_t, CmdAppIncrementalDone, (MessageParcel&, MessageParcel&));
+    MOCK_METHOD(int32_t, CmdGetIncrementalFileHandle, (MessageParcel&, MessageParcel&));
+    MOCK_METHOD(int32_t, CmdGetBackupInfo, (MessageParcel&, MessageParcel&));
+    MOCK_METHOD(int32_t, CmdUpdateTimer, (MessageParcel&, MessageParcel&));
+    MOCK_METHOD(int32_t, CmdUpdateSendRate, (MessageParcel&, MessageParcel&));
+    MOCK_METHOD(void, ServiceStubSupplement, ());
+    MOCK_METHOD(void, ServiceStubSuppAppendBundles, ());
+};
+} // namespace OHOS::FileManagement::Backup
+#endif // OHOS_FILEMGMT_BACKUP_SERVICE_STUB_MOCK_H
