6c17936427ee674177c6715ba6b05f03de29276f
chensihan <chensihan13@huawei.com>
2024-09-20 01:21:52 +0000

    update tests/unittests/backup_utils/b_jsonutil/b_jsonutil_other_test.cpp.
    Signed-off-by: chensihan <chensihan13@huawei.com>

--- tests/unittests/backup_utils/b_jsonutil/b_jsonutil_other_test.cpp
+++ tests/unittests/backup_utils/b_jsonutil/b_jsonutil_other_test.cpp
@@ -249,7 +249,7 @@ HWTEST_F(BJsonUtilTest, b_jsonutil_BuildBundleInfoJson_0402, testing::ext::TestS
             .WillOnce(Return(nullptr));
         EXPECT_CALL(*cJsonMock, cJSON_CreateArray())
             .WillOnce(Return(reinterpret_cast<cJSON *>(&cjson)))
-            .WillOnce(Return(nullptr));
+            .WillOnce(Return(reinterpret_cast<cJSON *>(&cjson)));
         EXPECT_CALL(*cJsonMock, cJSON_AddItemToObject(_, _, _))
             .WillOnce(Return(true))
             .WillOnce(Return(true));
