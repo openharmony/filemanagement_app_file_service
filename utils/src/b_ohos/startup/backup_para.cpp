f92bb7077cfbf89eaec3747e156c87674ac7a9ae
chensihan <chensihan13@huawei.com>
2024-10-14 08:19:03 +0000

    update utils/src/b_ohos/startup/backup_para.cpp.
    Signed-off-by: chensihan <chensihan13@huawei.com>

--- utils/src/b_ohos/startup/backup_para.cpp
+++ utils/src/b_ohos/startup/backup_para.cpp
@@ -38,9 +38,9 @@ const char* BACKUP_DEBUG_STATE = "sys.backup.check.enable";
 static tuple<bool, string> GetConfigParameterValue(const string &key, uint32_t len)
 {
     char configParam[] = "false";
-    int ret = GetParameter(key.c_str(), "", configParam, len + 1);
-    if (ret <= 0) {
-        HILOGE("Fail to GetParameter name = %{public}s, ret = %{public}d", key.c_str(), ret);
+    int length = GetParameter(key.c_str(), "", configParam, len + 1);
+    if (length <= 0) {
+        HILOGE("Fail to GetParameter name = %{public}s, length = %{public}d", key.c_str(), length);
         return {false, ""};
     }
     return {true, configParam};
