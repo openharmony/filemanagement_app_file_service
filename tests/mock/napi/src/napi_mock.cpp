d0131725eef5c9d39781753d51de39fe34ffbaaa
chensihan <chensihan13@huawei.com>
2024-10-23 03:07:26 +0000

    update tests/mock/napi/src/napi_mock.cpp.
    Signed-off-by: chensihan <chensihan13@huawei.com>

--- tests/mock/napi/src/napi_mock.cpp
+++ tests/mock/napi/src/napi_mock.cpp
@@ -18,6 +18,12 @@
 
 int uv_queue_work(uv_loop_t* loop, uv_work_t* req, uv_work_cb work_cb, uv_after_work_cb after_work_cb)
 {
+    if (work_cb) {
+        work_cb(req);
+    }
+    if (after_work_cb) {
+        after_work_cb(req, 0);
+    }
     return OHOS::FileManagement::Backup::Napi::napi->uv_queue_work(loop, req, work_cb, after_work_cb);
 }
 
