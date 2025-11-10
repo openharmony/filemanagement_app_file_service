a4723c572fa39ba50cb563989f47ba77c3e1af62
ZhangYuanhuai <zhangyuanhuai@huawei.com>
2025-11-10 12:54:59 +0800

    保活锁DFX
    Signed-off-by: ZhangYuanhuai <zhangyuanhuai@huawei.com>

--- tests/unittests/backup_sa/module_ipc/service_sub_test.cpp
+++ tests/unittests/backup_sa/module_ipc/service_sub_test.cpp
@@ -130,9 +130,9 @@ ErrCode ServiceSubTest::Init(IServiceReverseType::Scenario scenario)
     detailInfos.emplace_back(json);
     string errMsg;
     ErrCode ret = 0;
-    int calls_num = 2;
+    int callsNum = 2;
     if (scenario == IServiceReverseType::Scenario::RESTORE) {
-        EXPECT_CALL(*powerClientMock_, CreateRunningLock(_, _)).Times(calls_num)
+        EXPECT_CALL(*powerClientMock_, CreateRunningLock(_, _)).Times(callsNum)
             .WillRepeatedly(Return(nullptr));
         EXPECT_TRUE(servicePtr_ != nullptr);
         EXPECT_TRUE(remote_ != nullptr);
@@ -148,7 +148,7 @@ ErrCode ServiceSubTest::Init(IServiceReverseType::Scenario scenario)
         ret = servicePtr_->Finish();
         EXPECT_EQ(ret, BError(BError::Codes::OK));
     } else if (scenario == IServiceReverseType::Scenario::BACKUP) {
-        EXPECT_CALL(*powerClientMock_, CreateRunningLock(_, _)).Times(calls_num)
+        EXPECT_CALL(*powerClientMock_, CreateRunningLock(_, _)).Times(callsNum)
             .WillRepeatedly(Return(nullptr));
         sptr<IServiceReverse> srptr_ = static_cast<sptr<IServiceReverse>>(remote_);
         ret = servicePtr_->InitBackupSession(srptr_);
