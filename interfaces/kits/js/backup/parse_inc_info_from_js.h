b0e3703e7dddc87c29e7c7351f88a79ad7473411
zyh <zhangyuanhuai@huawei.com>
2025-12-20 17:39:06 +0800

    碎片清理需求
    Signed-off-by: zyh <zhangyuanhuai@huawei.com>

--- interfaces/kits/js/backup/parse_inc_info_from_js.h
+++ interfaces/kits/js/backup/parse_inc_info_from_js.h
@@ -29,8 +29,8 @@ public:
                               bool &isPreciseScan, std::vector<BIncrementalData> &bundleNames);
 
     static std::tuple<bool, int, unsigned int, unsigned int> ParseFsRequestConfig(const LibN::NVal &FsRequestConfig);
-    static bool VerifyFsRequestConfigParam(LibN::NFuncArg &funcArg,
-        int &triggerType, unsigned int &writeSize, unsigned int &waitTime, napi_env env);
+    static bool VerifyFsRequestConfigParam(napi_env env, LibN::NFuncArg &funcArg,
+        int &triggerType, unsigned int &writeSize, unsigned int &waitTime);
 };
 } // namespace OHOS::FileManagement::Backup
 #endif //INTERFACES_KITS_JS_SRC_MOD_BACKUP_PARSE_INCREMENT_INFO_FROM_JS_H
