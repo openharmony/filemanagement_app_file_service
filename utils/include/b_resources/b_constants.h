681678cd14f89c1bd3b52a55301ee7b0abaf8fa4
zyh <zhangyuanhuai@huawei.com>
2025-12-22 20:55:23 +0800

    新增接口filesystemServiceRequest
    Signed-off-by: zyh <zhangyuanhuai@huawei.com>

--- utils/include/b_resources/b_constants.h
+++ utils/include/b_resources/b_constants.h
@@ -165,7 +165,7 @@ static inline std::string EXTENSION_RESTORE = "restore";
 // 路径校验
 static inline std::string PATH_ABSOLUTE = "../";
 // 克隆应用bundleName
-static inline std::string BUNDLE_DATA_CLONE = ".dataclone";
+static inline std::string BUNDLE_DATA_CLONE = "com.huawei.hmos.dataclone";
 // 多用户场景应用备份数据路径
 static inline std::string GetSaBundleBackupDir(int32_t userId)
 {
