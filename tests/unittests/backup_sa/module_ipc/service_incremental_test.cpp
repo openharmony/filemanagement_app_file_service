bb06fb369d28975630b2dcc9e0d8db069de5ebf0
I_am__ok <lisu26@huawei.com>
2026-08-03 20:43:01 +0800

    update: 更新文件 service_incremental_test.cpp 修改代码
    Signed-off-by: I_am__ok <lisu26@huawei.com>

--- tests/unittests/backup_sa/module_ipc/service_incremental_test.cpp
+++ tests/unittests/backup_sa/module_ipc/service_incremental_test.cpp
@@ -1687,7 +1687,7 @@ HWTEST_F(ServiceIncrementalTest, SUB_ServiceIncremental_GetIncrementalFileHandle
  * @tc.desc: 测试 GetIncrementalFileHandle 的正常/异常分支
  * @tc.size: MEDIUM
  * @tc.type: FUNC
- *service_incremental_test.cpp @tc.level Level 1
+ * @tc.level Level 1
  * @tc.require: issueIAKC3I
  */
 HWTEST_F(ServiceIncrementalTest, SUB_ServiceIncremental_GetIncrementalFileHandle_0100, TestSize.Level1)
