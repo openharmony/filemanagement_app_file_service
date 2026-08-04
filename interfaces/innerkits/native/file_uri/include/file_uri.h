595b2ddb8d620747b7896ff90bfd13b7d8edbb9f
cnhook <xueshenke@h-partners.com>
2026-08-04 15:02:04 +0800

    update: 更新文件 file_uri.h
    Signed-off-by: cnhook <xueshenke@h-partners.com>

--- interfaces/innerkits/native/file_uri/include/file_uri.h
+++ interfaces/innerkits/native/file_uri/include/file_uri.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2024 Huawei Device Co., Ltd.
+ * Copyright (c) 2023 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -13,41 +13,32 @@
  * limitations under the License.
  */
 
-#ifndef OHOS_FILE_FILEURI_URI_FFI_H
-#define OHOS_FILE_FILEURI_URI_FFI_H
+#ifndef APP_FILE_SERVICE_FILE_URI_FILE_URI_H
+#define APP_FILE_SERVICE_FILE_URI_FILE_URI_H
 
 #include <string>
 
 #include "uri.h"
-#include "native/ffi_remote_data.h"
-#include "cj_ffi/cj_common_ffi.h"
-
 namespace OHOS {
-namespace CJSystemapi {
-namespace FileUri {
-
-class FileUriImpl : public OHOS::FFI::FFIData {
+namespace AppFileService {
+namespace ModuleFileUri {
+class FileUri {
 public:
-    explicit FileUriImpl(const std::string &uriOrPath);
-
     std::string GetName();
     std::string GetPath();
+    std::string GetRealPath();
+    std::string GetRealPathBySA(const std::string &targeBundleName = "");
     std::string ToString();
+    std::string GetFullDirectoryUri();
+    bool IsRemoteUri();
 
-    OHOS::FFI::RuntimeType* GetRuntimeType() override { return GetClassType(); }
-private:
+    bool CheckUriFormat(const std::string &uri);
+    explicit FileUri(const std::string &uriOrPath);
+    ~FileUri() = default;
+
     Uri uri_;
-
-    friend class OHOS::FFI::RuntimeType;
-    friend class OHOS::FFI::TypeBase;
-    static OHOS::FFI::RuntimeType* GetClassType()
-    {
-        static OHOS::FFI::RuntimeType runtimeType = OHOS::FFI::RuntimeType::Create<OHOS::FFI::FFIData>("FileUriImpl");
-        return &runtimeType;
-    }
 };
-
-}
-}  // namespace CJSystemapi
+}  // ModuleFileUri
+}  // namespace AppFileService
 }  // namespace OHOS
-#endif  // OHOS_FILE_FILEURI_URI_FFI_H
+#endif  // APP_FILE_SERVICE_FILE_URI_FILE_URI_H
