442ea3b5db6e9497e306f4cf3859d3abcae39405
dongtingchi <dongtingchi@huawei.com>
2026-09-16 11:03:35 +0800

    fix
    Signed-off-by: dongtingchi <dongtingchi@huawei.com>

--- tests/unittests/backup_sa/module_ipc/sub_service_test.cpp
+++ tests/unittests/backup_sa/module_ipc/sub_service_test.cpp
@@ -502,10 +502,11 @@ HWTEST_F(ServiceTest, SUB_Service_ExtConnectDied_0002, TestSize.Level1)
     auto callDied = [](const string &bundleName, bool isCleanCalled) {};
     auto callConnected = [](const string &bundleName) {};
     auto connectPtr = sptr(new SvcBackupConnection(callDied, callConnected, callName));
-   connectPtr->connState_ = SvcBackupConnection::ConnState::DISCONNECTED;
+    connectPtr->connState_ = SvcBackupConnection::ConnState::DISCONNECTED;
     EXPECT_CALL(*saUtils, IsSABundleName(_)).WillOnce(Return(true)).WillOnce(Return(true)).RetiresOnSaturation();
     EXPECT_CALL(*session, GetExtConnection(_)).WillRepeatedly(Return(wptr(connectPtr)));
     EXPECT_CALL(*connect, GetWasEverConnected()).WillOnce(Return(true)).RetiresOnSaturation();
+    EXPECT_CALL(*connect, IsExtAbilityConnected()).WillRepeatedly(Return(false));
     EXPECT_CALL(*session, GetClearDataFlag(_)).WillRepeatedly(Return(true));
     EXPECT_CALL(*session, GetIsRestoreEnd(_)).WillOnce(Return(false)).WillOnce(Return(false));
     EXPECT_CALL(*session, GetScenario()).WillOnce(Return(IServiceReverseType::Scenario::UNDEFINED))
